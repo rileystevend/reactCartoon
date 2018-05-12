2a5997fdf0e4858000960415272c33aaa767f7a1
Steven Riley <rileystevend@gmail.com>
2018-05-12 14:58:01 -0500

    v1

--- reactCartoon.js
+++ reactCartoon.js
@@ -1,9 +1,57 @@
-//reactCartoon coding challenge
-
 class Lamp extends React.Component {
+  constructor (props) {
+      super(props);
+      this.state = {
+        hover: false,
+        leftLocation: true,
+      };
+      //alert
 
+      this.toggleLight = this.toggleLight.bind(this);
+      this.flipLocation = this.flipLocation.bind(this);
+    };
+
+    toggleLight () {
+      this.setState({
+        hover: !this.state.hover
+      })
+    }
+
+    flipLocation () {
+      console.log(this.state.leftLocation)
+      this.setState({
+        leftLocation: !this.state.leftLocation,
+      })
+    }
   
     render () {
+      //styles
+      const {hover, leftLocation} = this.state;
+      const backgroundStyles = {
+        height: '500px',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'contain',
+        position: 'relative',
+        backgroundImage: `url("https://st.depositphotos.com/1000374/2802/i/950/depositphotos_28020609-stock-photo-graphical-sketch-of-an-interior.jpg")`,
+        zIndex: -1,
+      };
+      const lampStyles = {
+        height: '100px',
+        top: leftLocation ? '216px' : '350px',
+        left: leftLocation ? '113px' : '505px',
+        position: 'relative',
+        zIndex: 2,
+      };
+      const lightStyles = {
+        width: '75px',
+        top: leftLocation ? '191px' : '325px',
+        left: leftLocation ? '24px' : '420px',
+        position: 'relative',
+        opacity: hover ? 1 : this.props.lit ? 1 : 0.3,
+        transition: '3s',
+        transform: hover ? 'rotate(1turn)' : 'none',
+        zIndex: 1,
+      };
 
       return (
         <div style={backgroundStyles}>
