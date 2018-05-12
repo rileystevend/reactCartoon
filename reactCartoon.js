9f7d79a45c17ae5ba8bb2749df157e9867c08a06
Steven Riley <rileystevend@gmail.com>
2018-05-12 14:56:02 -0500

    react component skeleton

--- reactCartoon.js
+++ reactCartoon.js
@@ -1,1 +1,16 @@
 //reactCartoon coding challenge
+
+class Lamp extends React.Component {
+
+  
+    render () {
+
+      return (
+        <div style={backgroundStyles}>
+          <img src={'https://image.flaticon.com/icons/png/512/109/109968.png'} style={lampStyles} alt="lamp" onMouseEnter={this.toggleLight} onMouseLeave={this.toggleLight} onClick={this.flipLocation}/>
+          <img src={"https://www.clker.com/cliparts/s/8/X/x/k/m/sun-design-md.png"} style={lightStyles} />
+        </div>
+      )
+    }
+}
+export default Lamp
